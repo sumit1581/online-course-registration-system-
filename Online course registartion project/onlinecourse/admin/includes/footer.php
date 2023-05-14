7f1e4d54eedb9adf652768742e5bea2ce04d8325
sumit1581 <Sumitvishwambhar15@gmail.com>
2023-05-14 15:02:42 +0530

    This project is Developed using Php, Javascript, And MySql

---
+++ Online course registartion project/onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; <?php echo date('Y');?> Online Course Registration | By : <a href=" " target="_blank">Vishwambhar Sumit & Bodkhe Nilesh </a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
